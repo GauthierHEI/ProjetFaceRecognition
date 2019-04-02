6abe66fa994276d660bbd0cd843cf16bd131bd33
Loïc Desjacques <loic.desjacques@hei.yncrea.fr>
2019-04-02 14:42:33 +0200

    animated splash screen

--- app/src/main/java/com/example/projets8/SplashScreen.java
+++ app/src/main/java/com/example/projets8/SplashScreen.java
@@ -3,14 +3,25 @@ package com.example.projets8;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends Activity {
+
+    Animation fromtop;
+    ImageView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        title = (ImageView) findViewById(R.id.title);
+        fromtop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
+
+        title.setAnimation(fromtop);
 
         Thread timerThread = new Thread(){
             public void run(){
