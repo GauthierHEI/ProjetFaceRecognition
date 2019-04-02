210c8983883e2a3f42c0bf269ad427d87c20701b
Bapth_dej <baptiste.desjobert@hei.yncrea.fr>
2019-04-02 12:27:51 +0200

    ooook

--- app/src/main/java/com/example/projets8/MainActivity.java
+++ app/src/main/java/com/example/projets8/MainActivity.java
@@ -22,7 +22,6 @@ import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-//import java.util.Collections;
 import java.util.Collections;
 import java.util.Date;
 
